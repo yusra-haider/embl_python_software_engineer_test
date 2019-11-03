d664f60a5cc380bb2ede5e3020ff50fa3fcb067e
yusra-haider <yusra.haider@gmail.com>
2019-11-03 16:59:16 +0500

    made the tests optional in the bash script

--- build_test_deploy_docker_to_heroku.sh
+++ build_test_deploy_docker_to_heroku.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-heroku_app_name=$1
-./build_docker_image.sh
+run_tests=$1
+heroku_app_name=$2
+
+./build_docker_image.sh $1
 if [ $? -eq 0 ]
 then
     ./deploy_docker_to_heroku.sh ${heroku_app_name}
